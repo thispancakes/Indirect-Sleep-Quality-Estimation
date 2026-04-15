944fe8a8b770f98409288a567a9bbdc21663b435
MTPeraya <m.perayalsc@gmail.com>
2026-04-15 23:53:51 +0700

    add config example

---
+++ config_example.py
@@ -0,0 +1,11 @@
+# rename to config.py and fill in your database credentials
+
+# Database connection credentials
+DB_HOST = ""
+# Replace with your database host
+DB_USER = ""
+# Replace with your database username
+DB_PASSWD = ""
+# Replace with your database password
+DB_NAME = ""
+# Replace with your database name
