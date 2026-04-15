8b19bad13234b0ad1b65d2a5c2b24a44be2b60e4
MTPeraya <m.perayalsc@gmail.com>
2026-04-15 23:52:30 +0700

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Database connection credentials
-DB_HOST = "iot.cpe.ku.ac.th"
-# Replace with your database host
-DB_USER = "b6710545776"
-# Replace with your database username
-DB_PASSWD = "peraya.l@ku.th"
-# Replace with your database password
-DB_NAME = "b6710545776"
-# Replace with your database name
